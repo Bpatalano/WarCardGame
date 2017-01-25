f5320d8fde136cc287c812a3e3bfde87f26d4db8
Bpatalano <ben.patalano@gmail.com>
2017-01-24 20:42:52 -0800

    Updated test specifics and display
    -Tests were using references that were causing them to miss
    -War needed more tests for new methods
    -Before each hook wasn't needed in some tests

--- app/test/warGameTests.js
+++ app/test/warGameTests.js
@@ -1,5 +1,4 @@
-let expect = require('chai').expect;
-let mocha = require('mocha');
+let expect = chai.expect;
 
 describe ("War Game", () => {
   
@@ -7,77 +6,29 @@ describe ("War Game", () => {
 
     let testCard = new Card(3, 4);
 
-    it("has a rank", () => {
-      expect(testCard.rank).to.equal(4)
+    it("has a suit", () => {
+      
+      expect(testCard.suit).to.equal(3)
     });
 
-    it("has a suit", () => {
-      expect(testCard.suit).to.equal("hearts")
+    it("has a rank", () => {
+      
+      expect(testCard.rank).to.equal(4)
     });
 
   });
 
   describe("Deck", () => {
 
-    beforeEach(() => {
-      let testDeck = new Deck(4, 13);
-    });
+    let testDeck = new Deck(4, 13);
 
     describe("builds a full deck", () => {
+      
       it("has 52 cards", () => {
+        
         let count = testDeck.cardCount();
+        
         expect(count).to.equal(52);
-      });
-      it("has 4 suits", () => {
-        let suitsLeft = testDeck.suitsLeft();
-        expect(suitsLeft.length).to.equal(4);
-      });
-      it("has 13 ranks", () => {
-        let ranksLeft = testDeck.ranksLeft();
-        expect(ranksLeft).to.equal(13)
-      });
-    });
-
-    describe("shuffle", () => {
-      it("changes card locations", () => {
-        let randomNums = [];
-
-        for (let i = 0; i < 8; i ++) {
-          let num = Math.floor(Math.random() * 51);
-          randomNums.push(num);
-        };
-
-        let beforeShuffle = [];
-
-        randomNums.forEach((num) => {
-          beforeShuffle.push(Deck.getCardAt(num));
-        });
-
-        Deck.shuffle();
-
-        let afterShuffle = [];
-
-        randomNums.forEach((num) => {
-          afterShuffle.push(Deck.getCardAt(num));
-        });
-
-        expect(afterShuffle).to.not.deep.equal(beforeShuffle);
-      });
-    });
-
-    describe("deal", () => {
-
-      let testCard = testDeck.deal();
-
-      it("returns a card", () => {
-        expect(testCard).to.be.an.instanceOf(Card);
-      });
-
-      it("reduces size of deck by 1", () => {
-        let firstCount = testDeck.cardCount();
-        testDeck.deal();
-        let secondCount = testDeck.cardCount();
-        expect(firstCount).to.equal(secondCount + 1);
       });
     });
 
@@ -86,94 +37,148 @@ describe ("War Game", () => {
       let count = testDeck.cardCount();
 
       it("returns a number", () => {
+        
         expect(count).to.be.a('number');
       });
 
       it("is between 0 and the deck size", () => {
+        
         expect(count).to.be.within(0,53)
       });
     });
 
-    describe("suitsLeft", () => {
-      let suits = testDeck.suitsLeft();
+    describe("shuffle", () => {
+      
+      it("changes card locations", () => {
+        
+        let randomNums = [];
 
-      it("returns an array", () => {
-        expect(suits).to.be.an.instanceOf(Array);
-      });
+        for (let i = 0; i < 8; i ++) {
+          
+          let num = Math.floor(Math.random() * 51);
+          
+          randomNums.push(num);
+        };
 
-      it("the array is a list of strings", () => {
-        expect(suits[0]).to.be.a('string');
-      });
+        let beforeShuffle = [];
 
-      it("whose length is eqaul to suits left", () => {
-        expect(suits.length).to.equal(4);
-      });
-    });
+        randomNums.forEach((num) => {
+          
+          beforeShuffle.push(testDeck.getCardAt(num));
+        });
 
-    describe("ranksLeft", () => {
-      let count = testDeck.ranksLeft();
+        testDeck.shuffle();
 
-      it("returns a number", () => {
-        expect(count).to.be.a('number');
-      });
+        let afterShuffle = [];
 
-      it("is between 0 and the deck size", () => {
-        expect(count).to.be.within(0,14)
-      });
-    });
+        randomNums.forEach((num) => {
+          
+          afterShuffle.push(testDeck.getCardAt(num));
+        });
 
-    describe("getCardAt", () => {
-      let testCard = testDeck.getCardAt(16);
-
-      it("returns a card", () => {
-        expect(testCard).to.be.an.instanceOf(Card);
+        expect(afterShuffle).to.not.deep.equal(beforeShuffle);
       });
     });
 
     describe("receiveCard", () => {
+      
       let testCard = new Card(5, 4);
+      
+      let count = testDeck.cardCount();
+      
       testDeck.receiveCard(testCard);
+      
+      let newCount = testDeck.cardCount();
+      
       it("increases deck size", () => {
-        expect(testDeck.cardCount()).to.equal(53);
+
+        expect(newCount).to.equal(count +1);
       });
     });
+
+    describe("deal", () => {
+
+      let testCard = testDeck.deal();
+
+      it("returns a card", () => {
+        
+        expect(testCard).to.be.an.instanceOf(Card);
+      });
+
+      it("reduces size of deck by 1", () => {
+        
+        let firstCount = testDeck.cardCount();
+        
+        testDeck.deal();
+        
+        let secondCount = testDeck.cardCount();
+        
+        expect(firstCount).to.equal(secondCount + 1);
+      });
+    });
+
   });
 
   describe("War", () => {
 
-    beforeEach(() => {
-      let testWar = new War(4, 13, 2)
-    });
+    let testWar = new War(2, 4, 13);
 
     describe("playersLeft", () => {
+      
       let players = testWar.playersLeft();
 
       it("returns a number", () => {
+        
         expect(players).to.be.a('number');
       });
 
       it("is between 0 and the total number of players", () => {
-        expect(count).to.be.within(0,3)
+        
+        expect(players).to.be.within(0,3)
       });
     });
 
     describe("takeTurn", () => {
+
+      let beforeTurn = testWar.cardCounts().slice();
+
+      testWar.takeTurn();
+
+      let afterTurn = testWar.cardCounts().slice();
+
       it("causes cards to change hands", () => {
-        let beforeTurn = testWar.cardCounts();
-        testWar.takeTurn(),
-        let afterTurn = testWar.cardCounts();
 
         expect(beforeTurn).to.not.deep.equal(afterTurn);
       });
     });
 
     describe("cardCounts", () => {
+
+      let counts = testWar.cardCounts().slice();
+
       it("returns an array", () => {
-        expect(testWar.cardCounts).to.be.an.instanceOf(Array);
+
+        expect(counts).to.be.an.instanceOf(Array);
       });
       it("returned array has as many entries as the players", () => {
-        expect(testWar.cardCounts.length).to.equal(4);
+
+        expect(counts.length).to.equal(2);
       });
     });
+    describe("playGame", () => {
+
+      let winner = testWar.playGame();
+
+      let finalCounts = testWar.cardCounts();
+
+      it("continues until one player is left", () => {
+
+        expect(finalCounts.length).to.equal(1);
+      });
+      it("returns a winner", () => {
+
+        expect(winner).to.be.a('number').within(0,3);
+      });
+    })
   });
 });
